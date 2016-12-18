a54b0ea8bc7cead8017b3b1b0c6ca155bfb1e37b
EdgarAllenPoe <tomstoutster@gmail.com>
2016-12-18 19:07:11 -0300

    test2

--- gc.sh
+++ gc.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
+
+comment=$1
+
+if [[ -n "$comment" ]]; then
 git add *
-git commit -m "This is a test"
+git commit -m "$1"
 git push -u origin master
+echo finished with no errors.
+else
+    echo "must include a comment"
+fi
