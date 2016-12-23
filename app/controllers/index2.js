2c0655cc42f37c5a955df22f51fbfe8560c92ce8
EdgarAllenPoe <tomstoutster@gmail.com>
2016-12-22 23:47:57 -0300

    css work on logo

---
+++ app/controllers/index2.js
@@ -0,0 +1,52 @@
+import Ember from 'ember';
+
+export default Ember.Controller.extend({
+
+    actions: {
+        createDonation() {
+            var donation = this.store.createRecord("donation", {
+                name: this.get('name'),
+                mobile: this.get('mobile'),
+                email: this.get('email'),
+               tdate: this.get('tdate'),
+                amount: this.get('amount'),
+            });
+
+            donation.save();
+
+            this.set('name', '');
+            this.set('mobile', '');
+            this.set('email', '');
+            this.set('tdate', '');
+            this.set('amount', '');
+        },
+
+        deleteDonation(donation) {
+            donation.deleteRecord();
+            donation.save();
+        },
+    },
+
+    tdate: Ember.computed('', function() {
+
+        return new Date();
+    }),
+
+    donationTotal: Ember.computed('model.@each.amount', function() {
+
+        return this.get('model').reduce(function(acc, value) {
+            return acc + value.get('amount');
+        }, 0);
+    }),
+   
+    // backerTotal: Ember.computed('model.@each.amount', function() {
+
+    //     return this.length;
+
+    //     // return this.get('model').reduce(function(acc, value) {
+    //     //     return acc + value.get('amount');
+    //     // }, 0);
+    // }),
+
+    backerTotal: Ember.computed.alias('model.length'),
+});
